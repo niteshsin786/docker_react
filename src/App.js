0c908aeafa337a6f27fd6e5faa192d68df678da1
Nitesh Singh <nisingh@corp.qualys.com>
2020-02-27 15:58:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hi there
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          yes i am here
         </a>
       </header>
     </div>
